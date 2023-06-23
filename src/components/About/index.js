7d99fc26554103db9adf165e43222467fc7349ef
rshulrg8688 <rahulrg8688@gmail.com>
2023-06-23 09:59:32 +0000

    add home button in about Comoonent

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,5 @@
 // Write your code here
+import {Link} from 'react-router-dom'
 import Header from '../Header'
 
 const about = () => (
@@ -9,6 +10,9 @@ const about = () => (
       alt="About"
     />
     <h1>About</h1>
+    <Link to="/">
+      <button type="button">Home</button>
+    </Link>
   </div>
 )
 
